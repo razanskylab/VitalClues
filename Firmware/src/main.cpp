1af09873746af3e7c6aba4bd3713def82fae310d
Johannes Rebling <johannesrebling@gmail.com>
2019-09-05 15:14:50 +0200

    pcb and first simple firmware added

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,19 +1,15 @@
-// Thermistor Example #3 from the Adafruit Learning System guide on Thermistors
-// https://learn.adafruit.com/thermistor/overview by Limor Fried, Adafruit Industries
-// MIT License - please keep attribution and consider buying parts from Adafruit
+#include <Arduino.h>
 
-/********************************************************************/
+
 // include libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-/********************************************************************/
-// define variables and constants we will use
 #define ONE_WIRE_BUS 2 // temp. sensor connected on D2
 #define CONTROL_PIN 3 // relay controlled via D3
 
 // which analog pin to connect
-#define THERMISTORPIN A1
+#define THERMISTORPIN A0
 // resistance at 25 degrees C
 #define THERMISTORNOMINAL 10000
 // temp. for nominal resistance (almost always 25 C)
@@ -26,25 +22,32 @@
 // the value of the 'other' resistor
 #define SERIESRESISTOR 9970
 
-int samples[NUMSAMPLES];
-
-
+float maxPadTemp = 50;
 float currentTemp;
 bool heatPadOn = false;
 float targetTemperature = 37; // in Celcius, do not use more than 50!!!
-float analTemp;
-float tempDiff = 0; // in Celcius, do not use more than 50!!!
-// if temperature lower than that, we assume a faulty reading
+float analTemp; // temperature measured in anus
+float padTemp;
 float faultTemperature = -120;
-float lowLimit = targetTemperature - 1;
-float highLim = targetTemperature + 1;
-const float pValue = 100.0;
-int pwmValue = 0;
-/********************************************************************/
+
+// PID variables
+float err; // difference between measured and set point
+float previous_err = 0;
+float integral = 0;
+float derivative = 0;
+const unsigned char nInt = 20;
+float errContainer[nInt];
+unsigned char iErr; // runs from 1 to 10
+const float pValue = 90.0; // proportional part of controller
+const float dValue = 0; // differential part of controller
+const float iValue = 10; // integral part of controller
+
+float pwmValue = 0; // set initial heating to 0
+int iPwmValue = 0; // inted pwm value
+
 // create temperature sensor class member from library
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature TempSensor(&oneWire);
-
 
 void setup(void) {
   Serial.begin(9600);
@@ -53,24 +56,20 @@ void setup(void) {
   TempSensor.setResolution(12); // reduce resolution to reduce noise
   pinMode(CONTROL_PIN, OUTPUT); // set relay pin as digital output
   analogWrite(CONTROL_PIN, pwmValue);
+
+  // reset error container to 0s
+  for (iErr = 0; iErr < nInt; iErr++)
+    errContainer[iErr] = 0;
+
+  iErr = 0;
 }
 
-void loop(void) {
-
-  uint8_t i;
-  float average;
-
-  // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES; i++) {
-    samples[i] = analogRead(THERMISTORPIN);
-    delay(12);
-  }
-
-  // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES; i++) {
-    average += samples[i];
-  }
+// function to read out smaller sensor placed in anus of animal
+float getAnalogTemp(){
+  float average = 0;
+  // take N samples in a row and sum them up
+  for (int i = 0; i< NUMSAMPLES; i++)
+    average += analogRead(THERMISTORPIN);
   average /= NUMSAMPLES;
 
   // convert the value to resistance
@@ -83,27 +82,37 @@ void loop(void) {
   analTemp += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
   analTemp = 1.0 / analTemp;                 // Invert
   analTemp -= 273.15;                         // convert to C
+  return analTemp;
+}
 
+// function to read out digital sensor placed on pad
+float getDigitalTemp(){
   // update temperature readings.
   TempSensor.requestTemperatures();
   currentTemp = TempSensor.getTempCByIndex(0);
+  return currentTemp;
+}
 
-  // Serial.print("Digital readout ");
+void loop(void) {
 
-  // Serial.println(" *C");
+  analTemp = getAnalogTemp();
+  padTemp = getDigitalTemp();
 
-  // send data over serial for plotting
-  // Serial.print(currentTemp);
-  // Serial.print(" ");
-  // Serial.print(lowLimit); // plot low and high limit as constant line
-  // Serial.print(" ");
-  // Serial.print(highLim); // plot low and high limit as constant line
-  // Serial.print(" ");
-  // // also plot control signal to see when heat mat is heating
-  // Serial.println(heatPadOn * targetTemperature); //scale up for plotting
+  err = targetTemperature - analTemp; // calculate error
+  errContainer[iErr] = err;
+  if (iErr >= nInt)
+    iErr = 0;
+  // calculate integral over last 10 measurements
+  integral = 0;
+  for(unsigned char i = 0; i < nInt; i++)
+    integral += errContainer[i];
+  // integral = integral + err; // update integral value
 
-  tempDiff = targetTemperature - analTemp;
-  pwmValue = 30 + (tempDiff * pValue);
+  derivative = err - previous_err; // update derivative error
+  pwmValue = pValue * err + dValue * derivative + iValue * integral;
+  previous_err = err;
+
+  iPwmValue = pwmValue;
   if (pwmValue < 0){
     pwmValue = 0;
   }
@@ -112,11 +121,18 @@ void loop(void) {
     pwmValue = 255;
   }
 
+  // if pad termperature is too high, disable, otherwise we carbonize the mouse
+  if (padTemp > maxPadTemp)
+    pwmValue = 0;
+
   analogWrite(CONTROL_PIN, pwmValue);
 
   Serial.print(analTemp);
   Serial.print(" ");
+  Serial.print(padTemp);
+  Serial.print(" ");
   Serial.println(targetTemperature);
+  // Serial.println(targetTemperature);
 
 
   // delay(500); // no need to check more than 2x per second...
